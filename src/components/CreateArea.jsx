df9eaa8446c388e45abffc4eea93036f3099cd97
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-06-02 10:33:07 +0100

    Add rreadme, screenshots

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import Fab from "@mui/material/Fab";
 import Zoom from "@mui/material/Zoom";
-import Textarea from "./Textarea";
 
 export default function CreateArea(props) {
   const [note, setNote] = useState({ title: "", content: "" });
